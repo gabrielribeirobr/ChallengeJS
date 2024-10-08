8bf3e8a6c935ec89d29148e716f6210c11c598ce
gabrielribeiro <gabrielribeirotnc@gmail.com>
2024-10-08 17:59:27 +0100

    check result

---
+++ desafio.js
@@ -0,0 +1,16 @@
+function countDiamonds() {
+    let diamonds = 0;
+    let open = 0;
+    const sequence = document.getElementById('code').value;
+    const res = document.getElementById('res');
+
+    for (let i = 0; i < sequence.length; i++) {
+        if (sequence[i] == '<') {
+            open++;
+        } else if (sequence[i] == '>' && open > 0) {
+            open--;
+            diamonds++;
+        }
+    }
+    res.innerHTML = `Número de diamantes: ${diamonds}`
+}
